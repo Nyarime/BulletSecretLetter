c9eeff207febcb4bb4be693e56dbb9013362628c
乃月希 <49052765+Nyarime@users.noreply.github.com>
2021-01-26 21:43:53 +0800

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,6 +1,6 @@
 <?php
-$dns = 'mysql:host=127.0.0.1;dbname=privatenote';
-$user = 'privatenote';
-$pass = 'ZMhC4NDKabphmAKF';
+$dns = 'mysql:host=127.0.0.1;dbname=BulletSecretLetter';
+$user = 'BulletSecretLetter';
+$pass = 'demopasswd';
 $connection = new PDO( $dns, $user, $pass );
 ?>
