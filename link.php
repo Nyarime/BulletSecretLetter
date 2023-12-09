59ae89aba191ea8f83d54063c93e60336cafe441
乃月希 <admin@itxe.net>
2023-12-10 06:15:06 +0800

    去除多余部分

--- link.php
+++ link.php
@@ -15,49 +15,42 @@ return ;
 <html>
 
 <head>
-    <title>Read Your Bullet Secret Letter - Biu.LI</title>
+    <title>Bullet Secret Letter Viewer by NMQU.COM</title>
+    <link rel="shortcut icon" href="https://blog.naixi.net/usr/nya/favicon.ico">
     <link rel="stylesheet" href="./css/hack.css" />
     <link rel="stylesheet" href="./css/dark-grey.css" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
-    <meta itemprop="name" content="Biu.Li"/>
-    <meta itemprop="image" content="https://idc.moe/idcmoe.png" />
+    <meta itemprop="name" content="BSLViewer"/>
+    <meta itemprop="image" content="https://blog.naixi.net/usr/nya/logo_black.png" />
     <meta name="description" itemprop="description" content="One-time encrypted message self-destruct after reading." />
     <script src="js/clipboard.min.js"></script>
 </head>
 
 <body class="hack dark-grey">
   <div class="main container">
-    <h1>Bullet Secret Letter</h1>
-    <h2>Read Your Bullet Secret Letter</h2>
+    <h1>Bullet Secret Letter Viewer</h1>
+    <h2>An encrypted message can be readable in NMQU.COM</h2>
     <?php
-include_once("sql.php");
+    include_once("sql.php");
 include_once("function.php");
-if (isset($_POST["message"],$_POST["password"])){
-    $id_message = rtrim(base64_encode(md5(microtime())),"=");
-	$password = $_POST["password"];
-    if ($password==""){
-        $password=rtrim(base64_encode(microtime()),"=");
-    }
-	$message_link = "https://".$_SERVER['HTTP_HOST']."/link.php?id=".$id_message."&password=".$password;
-    saveMessage($connection, $id_message, $_POST["message"],$password);
-    echo '<h2>Bullet Secret Letter link created：<button class="btn">Copy</button></h2><div class="alert alert-info">'.htmlspecialchars($message_link).'</div><h2>QRCode：Save the QR code to share with friends!</h2><div class="alert alert-info"><img src="https://idc.moe/qrcode/api.php?url='.htmlspecialchars($message_link).'"></div><hr>The created link will be self-destructed after reading.<p>© 2021 <a href="https://idc.moe">IDC.MOE</a> All Rights Reserved. <a href="https://biu.li/create.php">Create New Message</a> - <a href="https://github.com/Nyarime/BulletSecretLetter">GitHub</a></p>';
-} else if (isset($_GET["id"])){
+if (isset($_GET["id"])){
   if (isset($_GET["password"])){
      
-        echo '<h2>Hey, you have a message：</h2><div id="warning" class="alert alert-info">****<br>*************<br>**********************</div>';
-        echo '<button type="submit" id="NO1" >Open it!</button>';
+        echo '<h2>Hey, this is an encrypted message：</h2><div id="warning" class="alert alert-info">****<br>*************<br>**********************</div>';
+        echo '<button type="submit" id="BSL" >Open it!</button>';
         echo '<p> <p/>';
         echo '<p>This message can only be read once after opening.</p>';
-        echo '<p>If you left after opening, this message will disappear forever!</p>';
-        echo '<p>© 2021 <a href="https://idc.moe">IDC.MOE</a> All Rights Reserved. <a href="https://biu.li/create.php">Create New Message</a> - <a href="https://github.com/Nyarime/BulletSecretLetter">GitHub</a></p>';
+        echo '<p>For your safety, please check Server Host('.$_SERVER['HTTP_HOST'].'), browser domain are nmqu.com before you open this message!!!</p>';
+        echo '<p>When you opening, this message will disappear forever!</p>';
+        echo '<p>© 2024 <a href="https://naixi.net">Nyarime</a>. All Rights Reserved. <a href="https://nmqu.com/create.php">Create New Message</a> - <a href="https://github.com/Nyarime/BulletSecretLetter">GitHub</a></p>';
          
     
       
   } else {
     $data = "Oops, this message password is missing!";
     $data=htmlspecialchars($data);
-    echo '<h2>Hey, you have a message：</h2><div id="warning" class="alert alert-info">'.str_replace("\r\n","<br>",$data).'</div><hr>Warning! This message will be self-destructed after reading.<p>© 2021 <a href="https://idc.moe">IDC.MOE</a> All Rights Reserved. <a href="https://biu.li/create.php">Create New Message</a> - <a href="https://github.com/Nyarime/BulletSecretLetter">GitHub</a></p>';
+    echo '<h2>Hey, you have a message：</h2><div id="warning" class="alert alert-info">'.str_replace("\r\n","<br>",$data).'</div><hr>Warning! This message will be self-destructed after reading.<p>© 2024 <a href="https://naixi.net">Nyarime</a>. All Rights Reserved. <a href="https://nmqu.com/create.php">Create New Message</a> - <a href="https://github.com/Nyarime/BulletSecretLetter">GitHub</a></p>';
       
   }
   
@@ -65,14 +58,14 @@ if (isset($_POST["message"],$_POST["password"])){
     ?>
       <hr>
       <p></p>
-      <p>© 2021 <a href="https://idc.moe">IDC.MOE</a> All Rights Reserved. <a href="https://biu.li">Biu.LI</a></p>
+      <p>© <?php echo date("Y");?> <a href="https://naixi.net">Nyarime</a>. All Rights Reserved. <a href="https://nmqu.com">NMQU.COM</a></p>
       <?php
 }
 ?>
   </div>
 </body>
 <script>
-    var user = document.getElementById('NO1');  //获取用户控件
+    var user = document.getElementById('BSL');  //获取用户控件
     user.onclick = function () {  //当用户离开当前文本框的时行验证
         //1.创建Ajax对象
         var xhr = new XMLHttpRequest();
@@ -82,9 +75,9 @@ if (isset($_POST["message"],$_POST["password"])){
                 
                     var warning = document.getElementById('warning');
                     warning.innerHTML = xhr.responseText ;
-                   document.getElementById('NO1').disabled = true;
+                   document.getElementById('BSL').disabled = true;
                    //按钮 不可点
-               var change_cancel = document.getElementById("NO1");
+               var change_cancel = document.getElementById("BSL");
                    change_cancel.style.display = "none";
                    //按钮 block可见 none 不可见
                    
@@ -92,10 +85,10 @@ if (isset($_POST["message"],$_POST["password"])){
 
             }
          //3.初始化一个url请求
-        var user = document.getElementById('NO1').value;
+        var user = document.getElementById('BSL').value;
         //var password = document.getElementById('password').value;
        // var data = 'icp='+user+'&password='+password; //生成post请求数据
-        var data = 'NO1='+user;
+        var data = 'BSL='+user;
         var url = window.location.href+"&ready=yes";
         //var url = '/sms.php';
         
